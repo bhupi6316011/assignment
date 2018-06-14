9eee3fc7141712c844d450453f0f4b10c83e4c4a
bhupi6316011 <bhupendradhirwan011@gmail.com>
2018-06-14 11:54:46 +0530

    f

---
+++ bhupi.py
@@ -0,0 +1,16 @@
+#thislist = ["apple", "banana", "cherry"]
+#thislist[1] = "mango"
+#print(thislist)
+
+
+# set
+#l=[1,2,4,5,1]
+#print(l)
+s=set([1,1,2,3,4,5])
+#print(s)
+#print(s.add(8))
+#print(s)
+#print(s.pop())
+#print(s)
+print(s.remove(3))
+print(s)
